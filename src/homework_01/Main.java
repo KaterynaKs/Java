edcee055750cac08075a2611d3cd95baec6c5a9f
KaterynaKs <131392942+KaterynaKs@users.noreply.github.com>
2024-09-25 09:52:18 +0200

    Homework #1

---
+++ src/homework_01/Main.java
@@ -0,0 +1,32 @@
+package homework_01;
+
+public class Main {
+    public static void main(String[] args) {
+//        TASK 1
+        char character = 'G';
+        int integer = 89;
+        byte byteValue = 4;
+        short shortValue = 56;
+        float floatValue = 4.7333436F;
+        double doubleValue = 4.355453532;
+        long longValue = 12121L;
+        System.out.println("char: " + character);
+        System.out.println("int: " + integer);
+        System.out.println("byte: " + byteValue);
+        System.out.println("float: " + floatValue);
+        System.out.println("double: " + doubleValue);
+        System.out.println("long: " + longValue);
+
+//        TASK 2
+
+        int value = 345;
+
+        //Option 1
+        System.out.println("Число " + value + " -> " + value/100 + ", " + value/10%10 + ", " + value%10);
+
+        //Option 2
+        String valueStr = Integer.toString(value);
+        System.out.println("Число " + value + " -> " + valueStr.charAt(0) + ", " + valueStr.charAt(1) + ", " + valueStr.charAt(2));
+
+    }
+}
